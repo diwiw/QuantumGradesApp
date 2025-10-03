f4eb7db5c2b6854824f6d7067326643d31df0975
diw_vmpc <dejwids@gmail.com>
2025-10-03 21:10:45 +0200

    ci: attach sample outputs as artifacts (#16)

--- include/Version.hpp
+++ include/Version.hpp
@@ -1,3 +1,3 @@
 #pragma once
-#define APP_VERSION "v0.6.0-19-gd7e311c-dirty"
-#define APP_BUILD_DATE "2025-10-03 17:50:15"
+#define APP_VERSION "v0.6.0-20-g0858708-dirty"
+#define APP_BUILD_DATE "2025-10-03 21:06:34"
