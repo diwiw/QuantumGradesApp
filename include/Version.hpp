c868a1ec7513fcc071b8359bc11447508d7bf4e3
diw_vmpc <dejwids@gmail.com>
2025-10-03 21:28:52 +0200

    chore(ci): fix CMakeLists to use FetchContent for spdlog+fmt in CI

--- include/Version.hpp
+++ include/Version.hpp
@@ -1,3 +1,3 @@
 #pragma once
-#define APP_VERSION "v0.6.0-20-g0858708-dirty"
-#define APP_BUILD_DATE "2025-10-03 21:06:34"
+#define APP_VERSION "v0.6.0-21-gf4eb7db-dirty"
+#define APP_BUILD_DATE "2025-10-03 21:26:24"
