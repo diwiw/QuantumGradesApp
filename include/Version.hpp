38a8640fa4604c1fd49e36c7d066e4f61159593f
diw_vmpc <dejwids@gmail.com>
2025-10-03 22:08:34 +0200

    fix(ci): Windows dependencies fixed

--- include/Version.hpp
+++ include/Version.hpp
@@ -1,3 +1,3 @@
 #pragma once
-#define APP_VERSION "v0.6.0-23-g4f0dae1-dirty"
-#define APP_BUILD_DATE "2025-10-03 21:51:16"
+#define APP_VERSION "v0.6.0-25-g81cb50e-dirty"
+#define APP_BUILD_DATE "2025-10-03 22:07:29"
