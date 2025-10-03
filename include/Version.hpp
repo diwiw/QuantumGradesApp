94ad456724e41d1396d0992299d8b265bd90ddd4
diw_vmpc <dejwids@gmail.com>
2025-10-03 21:52:48 +0200

    ci(windows): configure vcpkg toolchain for curl support

--- include/Version.hpp
+++ include/Version.hpp
@@ -1,3 +1,3 @@
 #pragma once
-#define APP_VERSION "v0.6.0-22-gc868a1e-dirty"
-#define APP_BUILD_DATE "2025-10-03 21:35:37"
+#define APP_VERSION "v0.6.0-23-g4f0dae1-dirty"
+#define APP_BUILD_DATE "2025-10-03 21:51:16"
