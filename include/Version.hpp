4f0dae167cf2319722cead2a7fe54bd757c67a5f
diw_vmpc <dejwids@gmail.com>
2025-10-03 21:36:53 +0200

    fix(ci): ensure fmt is fetched before spdlog to satisfy SPDLOG_FMT_EXTERNAL

--- include/Version.hpp
+++ include/Version.hpp
@@ -1,3 +1,3 @@
 #pragma once
-#define APP_VERSION "v0.6.0-21-gf4eb7db-dirty"
-#define APP_BUILD_DATE "2025-10-03 21:26:24"
+#define APP_VERSION "v0.6.0-22-gc868a1e-dirty"
+#define APP_BUILD_DATE "2025-10-03 21:35:37"
