bb8d1974c39bf9040b3432ad2eb6b078321ed8d5
diw_vmpc <dejwids@gmail.com>
2025-11-12 19:12:13 +0100

    fix(version): Single Point of Truth and adjustments to fix versioning of the system

--- src/cli/main_cli.cpp
+++ src/cli/main_cli.cpp
@@ -1,15 +1,7 @@
-#include "Version.hpp"
 #include "cli/AppCLI.hpp"
-#include <iostream>
 
 int main(int argc, char** argv)
 {
-    // === Header ===
-    std::cout << "===================================\n";
-    std::cout << " QuantumGradesApp Backtest\n";
-    std::cout << " Version: " << APP_VERSION << "\n";
-    std::cout << " Build date: " << APP_BUILD_DATE << "\n";
-    std::cout << "===================================\n\n";
     qga::cli::AppCLI app;
     return app.run(argc, argv);
 }
